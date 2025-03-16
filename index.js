b729c43ef1270dd555d7c0199600c8f37b9e34d6
Bal3e <komputersaya2019@gmail.com>
2025-03-16 09:02:28 +0700

    vercel config

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ app.post("/save", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
+export default app;
 
 // Fetch data dari db untuk dimasukkan ke dalam dataSites
 
